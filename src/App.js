bf30a5ecb51a1ce23dcd0545f0a39871da11bda9
fd-mailden <fd.mailden@gmail.com>
2021-12-08 22:44:28 +0200

    modal window

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Modal from "./Modal/Modal";
+import React,{useState} from 'react';
+
 
 function App() {
+const [modalActive, setModalActive] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick = {()=>setModalActive(true)}>modale window</button>
+
+                  <p>
+        Almost there! Add an email address so you can start uploading. Upload
+        Mastering Your tracks Pro Plans Creators on SoundCloud 0% of free
+        uploads used Try Pro Unlimited for unlimited uploads. Drag and drop your
+        tracks & albums here Файл не выбран Make a playlist when multiple files
+        are selected Privacy: Public Private Provide FLAC, WAV, ALAC, or AIFF
+        for highest audio quality. Learn more about lossless HD. Supported file
+        types and sizes ⁃ Upload troubleshooting tips ⁃ Copyright FAQs By
+        uploading, you confirm that your sounds comply with our Terms of Use and
+        you don't infringe anyone else's rights. Legal ⁃ Privacy ⁃ Cookie Policy
+        ⁃ Cookie Manager ⁃ Imprint ⁃ Creator Resources ⁃ Blog ⁃ Charts ⁃
+        Language: English (US) Skip to previous Play current Skip to next
+        Shuffle Repeat track Current time: 0 seconds0:00 Duration: 41 minutes 18
+        seconds41:18 Volume Toggle mute The Jazz Hop Café Current track: cherry
+        blossom. [lofi / jazzhop / anime mix]cherry blossom. [lofi / jazzhop /
+        anime mix]
+      </p>
+
+
+      <p>
+        Almost there! Add an email address so you can start uploading. Upload
+        Mastering Your tracks Pro Plans Creators on SoundCloud 0% of free
+        uploads used Try Pro Unlimited for unlimited uploads. Drag and drop your
+        tracks & albums here Файл не выбран Make a playlist when multiple files
+        are selected Privacy: Public Private Provide FLAC, WAV, ALAC, or AIFF
+        for highest audio quality. Learn more about lossless HD. Supported file
+        types and sizes ⁃ Upload troubleshooting tips ⁃ Copyright FAQs By
+        uploading, you confirm that your sounds comply with our Terms of Use and
+        you don't infringe anyone else's rights. Legal ⁃ Privacy ⁃ Cookie Policy
+        ⁃ Cookie Manager ⁃ Imprint ⁃ Creator Resources ⁃ Blog ⁃ Charts ⁃
+        Language: English (US) Skip to previous Play current Skip to next
+        Shuffle Repeat track Current time: 0 seconds0:00 Duration: 41 minutes 18
+        seconds41:18 Volume Toggle mute The Jazz Hop Café Current track: cherry
+        blossom. [lofi / jazzhop / anime mix]cherry blossom. [lofi / jazzhop /
+        anime mix]
+      </p>
+      <p>
+        Almost there! Add an email address so you can start uploading. Upload
+        Mastering Your tracks Pro Plans Creators on SoundCloud 0% of free
+        uploads used Try Pro Unlimited for unlimited uploads. Drag and drop your
+        tracks & albums here Файл не выбран Make a playlist when multiple files
+        are selected Privacy: Public Private Provide FLAC, WAV, ALAC, or AIFF
+        for highest audio quality. Learn more about lossless HD. Supported file
+        types and sizes ⁃ Upload troubleshooting tips ⁃ Copyright FAQs By
+        uploading, you confirm that your sounds comply with our Terms of Use and
+        you don't infringe anyone else's rights. Legal ⁃ Privacy ⁃ Cookie Policy
+        ⁃ Cookie Manager ⁃ Imprint ⁃ Creator Resources ⁃ Blog ⁃ Charts ⁃
+        Language: English (US) Skip to previous Play current Skip to next
+        Shuffle Repeat track Current time: 0 seconds0:00 Duration: 41 minutes 18
+        seconds41:18 Volume Toggle mute The Jazz Hop Café Current track: cherry
+        blossom. [lofi / jazzhop / anime mix]cherry blossom. [lofi / jazzhop /
+        anime mix]
+      </p>
+      <p>
+        Almost there! Add an email address so you can start uploading. Upload
+        Mastering Your tracks Pro Plans Creators on SoundCloud 0% of free
+        uploads used Try Pro Unlimited for unlimited uploads. Drag and drop your
+        tracks & albums here Файл не выбран Make a playlist when multiple files
+        are selected Privacy: Public Private Provide FLAC, WAV, ALAC, or AIFF
+        for highest audio quality. Learn more about lossless HD. Supported file
+        types and sizes ⁃ Upload troubleshooting tips ⁃ Copyright FAQs By
+        uploading, you confirm that your sounds comply with our Terms of Use and
+        you don't infringe anyone else's rights. Legal ⁃ Privacy ⁃ Cookie Policy
+        ⁃ Cookie Manager ⁃ Imprint ⁃ Creator Resources ⁃ Blog ⁃ Charts ⁃
+        Language: English (US) Skip to previous Play current Skip to next
+        Shuffle Repeat track Current time: 0 seconds0:00 Duration: 41 minutes 18
+        seconds41:18 Volume Toggle mute The Jazz Hop Café Current track: cherry
+        blossom. [lofi / jazzhop / anime mix]cherry blossom. [lofi / jazzhop /
+        anime mix]
+      </p>
+      <p>
+        Almost there! Add an email address so you can start uploading. Upload
+        Mastering Your tracks Pro Plans Creators on SoundCloud 0% of free
+        uploads used Try Pro Unlimited for unlimited uploads. Drag and drop your
+        tracks & albums here Файл не выбран Make a playlist when multiple files
+        are selected Privacy: Public Private Provide FLAC, WAV, ALAC, or AIFF
+        for highest audio quality. Learn more about lossless HD. Supported file
+        types and sizes ⁃ Upload troubleshooting tips ⁃ Copyright FAQs By
+        uploading, you confirm that your sounds comply with our Terms of Use and
+        you don't infringe anyone else's rights. Legal ⁃ Privacy ⁃ Cookie Policy
+        ⁃ Cookie Manager ⁃ Imprint ⁃ Creator Resources ⁃ Blog ⁃ Charts ⁃
+        Language: English (US) Skip to previous Play current Skip to next
+        Shuffle Repeat track Current time: 0 seconds0:00 Duration: 41 minutes 18
+        seconds41:18 Volume Toggle mute The Jazz Hop Café Current track: cherry
+        blossom. [lofi / jazzhop / anime mix]cherry blossom. [lofi / jazzhop /
+        anime mix]
+      </p>
+      <p>
+        Almost there! Add an email address so you can start uploading. Upload
+        Mastering Your tracks Pro Plans Creators on SoundCloud 0% of free
+        uploads used Try Pro Unlimited for unlimited uploads. Drag and drop your
+        tracks & albums here Файл не выбран Make a playlist when multiple files
+        are selected Privacy: Public Private Provide FLAC, WAV, ALAC, or AIFF
+        for highest audio quality. Learn more about lossless HD. Supported file
+        types and sizes ⁃ Upload troubleshooting tips ⁃ Copyright FAQs By
+        uploading, you confirm that your sounds comply with our Terms of Use and
+        you don't infringe anyone else's rights. Legal ⁃ Privacy ⁃ Cookie Policy
+        ⁃ Cookie Manager ⁃ Imprint ⁃ Creator Resources ⁃ Blog ⁃ Charts ⁃
+        Language: English (US) Skip to previous Play current Skip to next
+        Shuffle Repeat track Current time: 0 seconds0:00 Duration: 41 minutes 18
+        seconds41:18 Volume Toggle mute The Jazz Hop Café Current track: cherry
+        blossom. [lofi / jazzhop / anime mix]cherry blossom. [lofi / jazzhop /
+        anime mix]
+      </p>
+      <Modal active={modalActive} setActive={setModalActive}>
+          <p>oads used Try Pro Unlimited for unlimited uploads. Drag and drop your
+        tracks & albums here Файл не выбран Make a playlist when multiple files
+        are selected Privacy: Public Private Provide FLAC, WAV, ALAC, or AIFF
+        for highest audio quality. Learn more about lossless HD. Supported file
+        types and sizes ⁃ Upload troubleshooting tips ⁃ Copyright FAQs By
+        uploading, you confirm that your sounds comply with our Terms of Use and
+        you don't infringe anyone else's rights. Legal ⁃ Privacy ⁃ Cookie Policy
+        ⁃ Cookie Manager ⁃ Imprint ⁃ Creator Resources ⁃ Blog ⁃ Charts ⁃
+        Language: English (US) Skip to prev</p>
+      </Modal>
     </div>
   );
 }
